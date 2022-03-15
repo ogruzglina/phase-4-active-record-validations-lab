34a27deb0cce8ea3b1de6d9ca9d653d861b40414
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-15 11:31:05 -0400

    commit tree deb0c36ebd1af04773c8aa524ecb4d57e185ae4c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,  
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ] 
+    # i - (insensitive) makes the whole expression case-insensitive (for instance /aBc/i would match AbC)
+    # \d matches any single digit 
+    # /any_word/ - it will check only this sequence of characters between //
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title } # Returns true if none of the elements match the given block.
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
